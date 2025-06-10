c55bd7c48b57530e4b59fb19d34a3881fad94d7b
NaCryptoKe <Nahomcryptoketema@gmail.com>
2025-06-10 14:18:25 +0300

    Initial commit

---
+++ Variables.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+const int PASS = 6497;
+
+struct ATM
+{
+    int PIN;
+    double balance;
+    double deposit;
+    double withdraw;
+};
+
+int main()
+{
+    struct ATM user;
+    while (1)
+    {
+        printf("Enter your PIN: ");
+        scanf("%d", &user.PIN);
+        if (user.PIN == PASS)
+        {
+            scanf("%d", &user.balance);
+            printf("%d is your balance.", user.balance);
+        }
+        else
+        {
+            printf("Incorrect PIN \n");
+        }
+    }
+    
+    
+    return 0;
+}
